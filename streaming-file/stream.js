e80ee63ec946194fd32d7207d21c1fe73b8cbcc5
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-03-03 17:10:18 -0600

    streaming-file

---
+++ streaming-file/stream.js
@@ -0,0 +1,24 @@
+'use strict'; 
+const { createReadStream, createWriteStream, appendFile, writeFile } = require('fs');
+
+const fileArg = process.argv[2];
+const { Transform, Writable} = require('stream');
+const upperCase = Transform();
+const writeStream = Writable();
+
+upperCase._transform = (buffer, _, cb) => {
+  cb(null, buffer.toString().toUpperCase());
+}
+
+writeStream._write = (buffer, _, next) => {
+  // writeFile('languages_caps.json', buffer, (err) => {
+  writeFile(fileArg, buffer, (err) => {
+    if(err) throw err;
+    process.stdout.write('The data to write was added to the file!\n');
+  });
+  next();
+};
+
+createReadStream("languages.json")
+.pipe(upperCase)
+.pipe(writeStream);
