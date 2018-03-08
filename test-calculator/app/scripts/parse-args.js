158ba6270c8396fb97bb41b6fb38fa5e580a495c
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-03-08 16:30:22 -0600

    test/logic mvp

--- test-calculator/app/scripts/parse-args.js
+++ test-calculator/app/scripts/parse-args.js
@@ -2,18 +2,23 @@
 
 const printInstructions = () => {
   console.log("Please enter your calculation in one of the following formats: '1 + 2' or '1+2'");
-  return false;
+  process.exit();
 }
 
 module.exports = (array) => {
   const equation = {};
-  if(array.length != 1 && array.length != 3){
+  if(array.length != 3){
     printInstructions();
     return false;
+
+  } else if (array[1]!='*' && array[1]!='+' && array[1]!='-' && array[1]!='/'){
+    printInstructions();
+    return false;
+
   } else {
     equation.num1 = +array[0];
     equation.num2 = +array[2];
-    equation.operator = array[1];
+    equation.operator = array.includes('*')?'multiply' : array.includes('+')?'add' : array.includes('/')?'divide' : 'subtract';
   }
   return equation;
 }
