0737f2a168fbd0c3a43e7a7bdb21e3368373a9c5
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-03-12 16:25:10 -0500

    http-server mvp

--- HTTP-server/server01.js
+++ HTTP-server/server01.js
@@ -2,11 +2,9 @@ const http = require('http');
 const server = http.createServer();
 const { createReadStream } = require('fs');
 
+
 server.on('request', (req, res) => {
-  console.log('Request received for: ', req.url);
-  createReadStream('/Users/hkphillips42/workspace/exercises/Node-Basics/HTTP-server/index.html')
-  .pipe(
-    res.end('<h1>Hello World</h1>')
-  )
-})
+  let readableStream = createReadStream('/Users/hkphillips42/workspace/exercises/Node-Basics/HTTP-server/index.html');
+  readableStream.pipe(res);
+});
 server.listen(8081);
